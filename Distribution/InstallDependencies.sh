fbbe2f0ff2fa6ae6bcdc20c4420a32bd78d2755f
Александр Клёмин <hanckwell@gmail.com>
2020-09-14 23:46:42 +0300

    InstallDependencies.sh added.

---
+++ Distribution/InstallDependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+base_directory="$(dirname "$0")"
+
+original="$base_directory/.."
+
+export PATH="$PATH:/usr/local/bin"
+export LANG="en_US.UTF-8"
+
+cocoapods_version="1.8.4"
+
+sudo gem install cocoapods -v "$cocoapods_version"
+sudo gem install cocoapods-binary -v 0.4.4
+
+pushd "$original" > /dev/null
+rm -rf Pods
+rm -f Podfile.lock
+pod _"$cocoapods_version"_ cache clean --all
+pod _"$cocoapods_version"_ install --repo-update
+
+if [ $? -ne 0 ]; then
+  exit $?
+fi
+
+popd > /dev/null
