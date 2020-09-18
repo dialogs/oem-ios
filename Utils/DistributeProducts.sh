2c1caf6938230ff573a6f46156adedc713d86d5f
Александр Клёмин <hanckwell@gmail.com>
2020-09-19 01:53:19 +0300

    DistributeProducts.sh corrected.

--- Utils/DistributeProducts.sh
+++ Utils/DistributeProducts.sh
@@ -6,4 +6,5 @@
 #  Created by Александр Клёмин on 19.09.2020.
 #  Copyright © 2020 DIALOG. All rights reserved.
 
+rm -rf "$PROJECT_DIR/$PRODUCT_NAME.framework"
 cp -r "$CONFIGURATION_BUILD_DIR/$CONTENTS_FOLDER_PATH" "$PROJECT_DIR/$PRODUCT_NAME.framework"
