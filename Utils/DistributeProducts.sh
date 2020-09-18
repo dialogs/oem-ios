8d4b0d5c1948517f98e422162d9fcd7eb4a5aa16
Александр Клёмин <hanckwell@gmail.com>
2020-09-19 01:22:16 +0300

    DistributeProducts.sh added.

---
+++ Utils/DistributeProducts.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+#  DistributeProducts.sh
+#  WhiteLabel
+#
+#  Created by Александр Клёмин on 19.09.2020.
+#  Copyright © 2020 DIALOG. All rights reserved.
+
+cp -r "$CONFIGURATION_BUILD_DIR/$CONTENTS_FOLDER_PATH" "$PROJECT_DIR/$PRODUCT_NAME.framework"
