58d621972b5efae9329685662a872a0062a6d3f2
Александр Клёмин <hanckwell@gmail.com>
2020-09-30 09:03:51 +0300

    DialogNotificationService product added to repository.

---
+++ Products/DialogNotificationService.framework/Headers/Dialog.h
@@ -0,0 +1,19 @@
+//
+//  Dialog.h
+//  Dialog
+//
+//  Created by Александр Клёмин on 28.08.2020.
+//  Copyright © 2020 DIALOG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Dialog.
+FOUNDATION_EXPORT double DialogVersionNumber;
+
+//! Project version string for Dialog.
+FOUNDATION_EXPORT const unsigned char DialogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dialog/PublicHeader.h>
+
+
