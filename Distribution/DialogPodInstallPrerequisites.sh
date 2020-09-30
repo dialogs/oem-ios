efff8e359906973840dd73a9f7a88a13e48bed5d
Александр Клёмин <hanckwell@gmail.com>
2020-09-30 11:26:20 +0300

    DialogPodInstallPrerequisites.sh corrected.

--- Distribution/DialogPodInstallPrerequisites.sh
+++ Distribution/DialogPodInstallPrerequisites.sh
@@ -5,5 +5,7 @@ base_directory="$(dirname "$0")"
 dialog_ios_directory="$base_directory/../Products/Dialog.framework/Frameworks/Dialog_iOS.framework"
 dialog_ios_archive="Dialog_iOS.zip"
 
-unzip -oq "$dialog_ios_directory/$dialog_ios_archive" -d "$dialog_ios_directory"
-rm "$dialog_ios_directory/$dialog_ios_archive"
+if [ -d "$dialog_ios_directory" ]; then
+    unzip -oq "$dialog_ios_directory/$dialog_ios_archive" -d "$dialog_ios_directory"
+    rm "$dialog_ios_directory/$dialog_ios_archive"
+fi
