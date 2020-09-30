b35bd16bf62dda49cea7125d1c0ddf43ec860a47
Александр Клёмин <hanckwell@gmail.com>
2020-09-30 09:04:51 +0300

    DistributeProduct.sh corrected.

--- Utils/DistributeProduct.sh
+++ Utils/DistributeProduct.sh
@@ -13,3 +13,13 @@ mkdir "$products_directory"
 rm -rf "$product_path"
 cp -r "$CONFIGURATION_BUILD_DIR/$CONTENTS_FOLDER_PATH" "$product_path"
 rm "$product_path/DistributeProduct.sh"
+
+dialog_ios="Dialog_iOS"
+dialog_ios_path="$product_path/Frameworks/$dialog_ios.framework"
+
+if [ -d "$dialog_ios_path" ]; then
+    pushd "$dialog_ios_path" > /dev/null
+        zip -q "$dialog_ios.zip" "$dialog_ios"
+        rm "$dialog_ios"
+    popd > /dev/null
+fi
