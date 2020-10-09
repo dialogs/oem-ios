4cd5d4ee46e559d49979635d10d1cad9464f648f
Александр Клёмин <hanckwell@gmail.com>
2020-10-09 11:28:49 +0300

    Dialog family products updated.

--- Products/Dialog.framework/Headers/Dialog-Swift.h
+++ Products/Dialog.framework/Headers/Dialog-Swift.h
@@ -188,7 +188,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
-@import Dialog_iOS;
 #endif
 
 #pragma clang diagnostic ignored "-Wproperty-attribute-mismatch"
@@ -205,12 +204,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="Dialog",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
 # pragma pop_macro("any")
 #endif
-
-
-SWIFT_CLASS("_TtC6Dialog17DialogAppDelegate")
-@interface DialogAppDelegate : CommonAppDelegate
-- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
-@end
 
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
