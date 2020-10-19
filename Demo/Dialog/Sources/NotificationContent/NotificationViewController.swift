ecf34979470c1bb62071df6ca9e50b6a14f6ec77
Dmitry Tikhonov <d.tihonov@dlg.im>
2020-10-19 16:42:10 +0700

    feature(oem): Added notification content extension

--- Demo/Dialog/Sources/NotificationContent/NotificationViewController.swift
+++ Demo/Dialog/Sources/NotificationContent/NotificationViewController.swift
@@ -6,18 +6,24 @@
 //  Copyright © 2020 DIALOG. All rights reserved.
 //
 
-import UIKit
-import DialogNotificationContentExtension
-import DialogSharedComponents
+import UserNotificationsUI
+import DialogNotificationContent
 
-class NotificationViewController: DialogNotificationContentExtensionViewController {
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
-    override var bubbleColors: KeyValuePairs<DialogSharedComponents.Theme.Keys.MessageBubbles, UIColor> {
-        return [ .myMessageBackgroundColor: #colorLiteral(red: 0.5960784314, green: 0.5333333333, blue: 0.768627451, alpha: 1) ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        DialogNotificationContent.configure(keychainGroup: Bundle.main.object(forInfoDictionaryKey: "Keychain access group") as? String,
+                                            appGroup: (Bundle.main.object(forInfoDictionaryKey: "CFBundleIdentifier") as? String).flatMap({ "group." + ($0 as NSString).deletingPathExtension }))
+        DialogNotificationContent.shared.embedViewConroller(in: self)
     }
 
-    override var avatarColors: [UIColor] {
-        return [#colorLiteral(red: 0.6980392157, green: 0.6588235294, blue: 0.8117647059, alpha: 1), #colorLiteral(red: 0.9411764706, green: 0.8156862745, blue: 0.368627451, alpha: 1), #colorLiteral(red: 0.8509803922, green: 0.6352941176, blue: 0.7764705882, alpha: 1), #colorLiteral(red: 0.4745098039, green: 0.7803921569, blue: 0.8196078431, alpha: 1), #colorLiteral(red: 0.9254901961, green: 0.6941176471, blue: 0.6941176471, alpha: 1), #colorLiteral(red: 0.5176470588, green: 0.6784313725, blue: 0.8549019608, alpha: 1), #colorLiteral(red: 0.9176470588, green: 0.5529411765, blue: 0.5294117647, alpha: 1), #colorLiteral(red: 0.662745098, green: 0.8470588235, blue: 0.6862745098, alpha: 1)]
+    func didReceive(_ notification: UNNotification) {
+        DialogNotificationContent.shared.didReceive(notification)
     }
-    
+
+    func didReceive(_ response: UNNotificationResponse, completionHandler completion: @escaping (UNNotificationContentExtensionResponseOption) -> Void) {
+        DialogNotificationContent.shared.didReceive(response, completionHandler: completion)
+    }
 }
---
+++ Sources/NotificationContent/DialogNotificationContent.swift
@@ -0,0 +1,88 @@
+//
+//  DialogNotificationService.swift
+//  DialogNotificationContent
+//
+//  Created by Dmitry Tikhonov on 14.10.2020.
+//  Copyright © 2020 DIALOG. All rights reserved.
+//
+
+import UserNotificationsUI
+import DialogNotificationContentExtension
+
+final public class DialogNotificationContent: NSObject, UNNotificationContentExtension {
+
+    public static let shared = DialogNotificationContent()
+
+    private var notificationViewController: NotificationContentExtensionViewController?
+
+    private override init(){}
+
+    public static func configure(keychainGroup: String?, appGroup: String?) {
+        self.shared.notificationViewController = NotificationContentExtensionViewController(keychainGroup: keychainGroup, appGroup: appGroup)
+    }
+
+    public func didReceive(_ notification: UNNotification) {
+        notificationViewController?.didReceive(notification)
+    }
+
+    public func didReceive(_ response: UNNotificationResponse, completionHandler completion: @escaping (UNNotificationContentExtensionResponseOption) -> Void) {
+        notificationViewController?.didReceive(response, completionHandler: completion)
+    }
+
+    public func embedViewConroller(in containerViewController: UIViewController) {
+        guard let controller = notificationViewController else {
+            return
+        }
+        if notificationViewController?.view.superview != nil {
+            notificationViewController?.removeFromParent()
+            notificationViewController?.view.removeFromSuperview()
+        }
+        containerViewController.addChild(controller)
+        controller.view.frame = containerViewController.view.bounds
+        controller.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        containerViewController.view.addSubview(controller.view)
+        controller.didMove(toParent: containerViewController)
+
+        controller.addObserver(self, forKeyPath: "preferredContentSize", options: .new, context: nil)
+    }
+
+    public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "preferredContentSize",
+           let value = change?[.newKey] as? CGSize,
+           let controller = (object as? NotificationContentExtensionViewController),
+           let parent = controller.parent {
+            DispatchQueue.main.async {
+                parent.preferredContentSize = value
+                parent.view.setNeedsLayout()
+                parent.view.setNeedsUpdateConstraints()
+            }
+        }
+    }
+
+}
+
+fileprivate class NotificationContentExtensionViewController: DialogNotificationContentExtensionViewController {
+
+    var extensionKeychainGroup: String? = nil
+
+    var extensionAppGroup: String? = nil
+
+    init(keychainGroup: String?, appGroup: String?) {
+        self.extensionKeychainGroup = keychainGroup
+        self.extensionAppGroup = appGroup
+        super.init()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override var appGroup: String? {
+        return extensionAppGroup
+    }
+
+    override var keychainGroup: String? {
+        return extensionKeychainGroup
+    }
+}
+
