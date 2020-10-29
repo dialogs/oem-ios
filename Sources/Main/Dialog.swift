b9467a36d48130d11a395b136067b18ff446663f
Dmitry Tikhonov <d.tihonov@dlg.im>
2020-10-29 19:47:25 +0700

    Added calls feature flags

--- Sources/Main/Dialog.swift
+++ Sources/Main/Dialog.swift
@@ -82,7 +82,12 @@ public class Dialog {
         Log.debug("Dialog configuration started")
         Self.shared.config = config
         Self.shared.container.register(DialogFeatureFlagsState.self) { _ in
-            return DialogFeatureFlagsState(featureFlags: config.defaultFeatureFlags)
+            let oemFeatureFlags = [DialogFeatureFlag(key: .callsEnabled, value: false),
+                                   DialogFeatureFlag(key: .videoCallsEnabled, value: false),
+                                   DialogFeatureFlag(key: .audioCallsEnabled, value: false),
+                                   DialogFeatureFlag(key: .isConferenceEnabled, value: false)]
+            return DialogFeatureFlagsState(featureFlags: oemFeatureFlags)
+                .applying(newState: DialogFeatureFlagsState(featureFlags: config.defaultFeatureFlags))
         }
         Self.shared.container.register(DialogAppGroupConfig.self) { _ in
             guard let appGroup = config.appGroup else {
