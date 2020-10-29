b7c0ed191f38747854654f14513231ff6b5d39cd
Александр Клёмин <hanckwell@gmail.com>
2020-10-29 21:36:11 +0300

    Forks added to git.

---
+++ Dependencies/Forks/Dialog_gRPC_Objc.framework/Headers/Dialog-gRPC-Objc-umbrella.h
@@ -0,0 +1,82 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Annotations.pbobjc.h"
+#import "Authentication.pbobjc.h"
+#import "Clickroad.pbobjc.h"
+#import "ConfigSync.pbobjc.h"
+#import "Contacts.pbobjc.h"
+#import "Counters.pbobjc.h"
+#import "Crypto.pbobjc.h"
+#import "Definitions.pbobjc.h"
+#import "Descriptor.pbobjc.h"
+#import "DeviceInfo.pbobjc.h"
+#import "EventBus.pbobjc.h"
+#import "GatewayService.pbobjc.h"
+#import "Groups.pbobjc.h"
+#import "HTTP.pbobjc.h"
+#import "Integrations.pbobjc.h"
+#import "MediaAndFiles.pbobjc.h"
+#import "Messaging.pbobjc.h"
+#import "Miniappsregistry.pbobjc.h"
+#import "Miscellaneous.pbobjc.h"
+#import "Obsolete.pbobjc.h"
+#import "Peers.pbobjc.h"
+#import "Permissions.pbobjc.h"
+#import "Presence.pbobjc.h"
+#import "Privacy.pbobjc.h"
+#import "Profile.pbobjc.h"
+#import "Push.pbobjc.h"
+#import "PushContent.pbobjc.h"
+#import "RawApi.pbobjc.h"
+#import "Reactions.pbobjc.h"
+#import "Registration.pbobjc.h"
+#import "Scalapb.pbobjc.h"
+#import "Search.pbobjc.h"
+#import "SequenceAndUpdates.pbobjc.h"
+#import "Spaces.pbobjc.h"
+#import "Stickers.pbobjc.h"
+#import "Threads.pbobjc.h"
+#import "Users.pbobjc.h"
+#import "WebRtc.pbobjc.h"
+#import "Authentication.pbrpc.h"
+#import "Clickroad.pbrpc.h"
+#import "ConfigSync.pbrpc.h"
+#import "Contacts.pbrpc.h"
+#import "Crypto.pbrpc.h"
+#import "DeviceInfo.pbrpc.h"
+#import "EventBus.pbrpc.h"
+#import "Groups.pbrpc.h"
+#import "Integrations.pbrpc.h"
+#import "MediaAndFiles.pbrpc.h"
+#import "Messaging.pbrpc.h"
+#import "Miniappsregistry.pbrpc.h"
+#import "Obsolete.pbrpc.h"
+#import "Permissions.pbrpc.h"
+#import "Presence.pbrpc.h"
+#import "Privacy.pbrpc.h"
+#import "Profile.pbrpc.h"
+#import "Push.pbrpc.h"
+#import "RawApi.pbrpc.h"
+#import "Reactions.pbrpc.h"
+#import "Registration.pbrpc.h"
+#import "Search.pbrpc.h"
+#import "SequenceAndUpdates.pbrpc.h"
+#import "Spaces.pbrpc.h"
+#import "Stickers.pbrpc.h"
+#import "Threads.pbrpc.h"
+#import "Users.pbrpc.h"
+#import "WebRtc.pbrpc.h"
+
+FOUNDATION_EXPORT double Dialog_gRPC_ObjcVersionNumber;
+FOUNDATION_EXPORT const unsigned char Dialog_gRPC_ObjcVersionString[];
+
