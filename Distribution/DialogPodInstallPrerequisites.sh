17530b2cff923b9d619baf3243ebd451a793dc06
Александр Клёмин <hanckwell@gmail.com>
2020-11-08 19:21:13 +0300

    DialogPodInstallPrerequisites.sh corrected.

--- Distribution/DialogPodInstallPrerequisites.sh
+++ Distribution/DialogPodInstallPrerequisites.sh
@@ -2,10 +2,25 @@
 
 base_directory="$(dirname "$0")"
 
-dialog_ios_directory="$base_directory/../Products/Dialog.framework/Frameworks/Dialog_iOS.framework"
+dialog_ios_directory="$base_directory/../Dependencies/Internal/Dialog_iOS.framework"
 dialog_ios_archive="Dialog_iOS.zip"
 
 if [ -d "$dialog_ios_directory" ]; then
     unzip -oq "$dialog_ios_directory/$dialog_ios_archive" -d "$dialog_ios_directory"
     rm "$dialog_ios_directory/$dialog_ios_archive"
+fi
+
+
+
+
+forks_directory="$base_directory/../Dependencies/Forks"
+
+if [ -d "$forks_directory" ]; then
+    webrtc_version="27.08.2020-15-07"
+    webrtc_archive="WebRTC-$webrtc_version.framework.zip"
+
+    curl -OL "https://dialog-ios-cdn.s3.eu-west-2.amazonaws.com/$webrtc_archive"
+
+    unzip -oq "$webrtc_archive" -d "$forks_directory"
+    rm "$webrtc_archive"
 fi
