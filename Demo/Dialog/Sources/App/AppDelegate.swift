fed02852b091575dde07076357cf2699ac032c24
Александр Клёмин <hanckwell@gmail.com>
2020-11-16 23:31:09 +0300

    DialogDemo AppDelegate minor correction applied.

--- Demo/Dialog/Sources/App/AppDelegate.swift
+++ Demo/Dialog/Sources/App/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  WhiteLabel
+//  DialogDemo
 //
-//  Created by Alexander Klemin on 07.01.2020.
+//  Created by Александр Клёмин on 07.09.2020.
 //  Copyright © 2020 DIALOG. All rights reserved.
 //
 
@@ -14,38 +14,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    var badgeUpdateHolder: Any? = nil
-    var routeLoggingHolder: Any? = nil
+    var badgeUpdateHolder: Any?
+    var routeLoggingHolder: Any?
 
-    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         var sharedAccessConfig: DialogSharedAccessConfig?
         
         if let appGroup = Bundle.main.object(forInfoDictionaryKey: "App group") as? String, let keychainGroup = Bundle.main.object(forInfoDictionaryKey: "Keychain access group") as? String {
             sharedAccessConfig = DialogSharedAccessConfig(appGroup: appGroup, keychainGroup: keychainGroup)
         }
 
-        Dialog.configure(with: Dialog.Config(endpoint: "grpc-oem-01.apps.sandbox.dlg.im",
-                                             apnsAppId: 100101,
-                                             sharedAccessConfig: sharedAccessConfig),
-                         style: DialogStyle(corporateColor: #colorLiteral(red: 0.5960784314, green: 0.5333333333, blue: 0.768627451, alpha: 1)))
+        Dialog.configure(
+            with: Dialog.Config(endpoint: "grpc-oem-01.apps.sandbox.dlg.im", apnsAppId: 100101, sharedAccessConfig: sharedAccessConfig),
+            style: DialogStyle(corporateColor: #colorLiteral(red: 0.5960784314, green: 0.5333333333, blue: 0.768627451, alpha: 1))
+        )
         
         badgeUpdateHolder = NotificationCenter.default
-            .addObserver(forName: Dialog.DialogDidUpdateBadgesStateNotification,
-                         object: nil,
-                         queue: .main,
-                         using: { notification in
-                            let userInfoKey = Dialog.BadgesState.notificationUserInfoKey
-                            var result = 0
-                            if let state = notification.userInfo?[userInfoKey] as? Dialog.BadgesState {
-                                result = state.unreadDialogs
-                            }
-                            UIApplication.shared.applicationIconBadgeNumber = result
-                         })
-        
+            .addObserver(forName: Dialog.DialogDidUpdateBadgesStateNotification, object: nil, queue: .main, using: { notification in
+                let userInfoKey = Dialog.BadgesState.notificationUserInfoKey
+                var result = 0
+                        
+                if let state = notification.userInfo?[userInfoKey] as? Dialog.BadgesState {
+                    result = state.unreadDialogs
+                }
+                        
+                UIApplication.shared.applicationIconBadgeNumber = result
+            })
         
         routeLoggingHolder = NotificationCenter.default
-            .addObserver(forName: .DialogCoordinationNotification, object: nil, queue: nil, using: { (notification) in
+            .addObserver(forName: .DialogCoordinationNotification, object: nil, queue: nil, using: { notification in
                 let key = Notification.DialogCoordinationUserInfoRouteKey
+                
                 if let route = notification.userInfo?[key] as? DialogGlobalRoute {
                     print("Dialog routed to \(route)")
                 } else {
@@ -64,9 +63,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Dialog.shared.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
     }
 
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         if Dialog.shared.canHandlePushNotificationWith(userInfo: userInfo) {
             Dialog.shared.application(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
         }
     }
+    
 }
